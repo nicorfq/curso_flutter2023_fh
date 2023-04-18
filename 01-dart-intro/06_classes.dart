633476e454b79689031537696db121e7407975c9
zero <zero@zeros-Mac-mini.local>
2023-04-18 19:57:41 -0400

    terminada seccion 1

---
+++ 01-dart-intro/06_classes.dart
@@ -0,0 +1,19 @@
+void main() {
+  final Hero wolverine = new Hero('Logan', 'Regeneracion');
+
+  print(wolverine);
+  print(wolverine.name);
+  print(wolverine.power);
+}
+
+class Hero {
+  String name;
+  String power;
+  
+  Hero( this.name, this.power );
+  
+//  Hero( String pName, String pPower ) 
+//    : name = pName,
+//      power = pPower;
+  
+}
---
+++ 01-dart-intro/13_try_catch_on_finally.dart
@@ -0,0 +1,30 @@
+void main() async {
+  print('Inicio del programa');
+  
+  try {
+    final value = await httpGet('https://fernando-herrera.com/cursos'); 
+    print( 'exito: $value' );
+    
+  } on Exception catch(err) {
+    print('Tenemos una Exception: $err');
+    
+  } catch (err) {
+    print('OOP!! algo terrible pasó: $err');
+  } finally {
+    print('Fin del try y catch');
+  }
+  
+  print('Fin del programa');
+}
+
+Future<String> httpGet( String url ) async {
+  
+  await Future.delayed( const Duration(seconds: 1));
+  
+  // throw new Exception('No hay parámetros en el URL');
+  
+  throw 'Error en la petición';
+  // return 'Tenemos un valor de la petición';
+  
+}
+
